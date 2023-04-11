824768821c219b1425c73f2ceb42a5e6a4580748
nick <10249960@student.sae.edu.au>
2023-04-11 16:19:01 +0800

    radio

---
+++ Assets/Asstes/Radio Controller System/RadioInputExample.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// This script is not a part of the actual system, it's just an example I have created to demonstrate
+/// how you can modify the radio station using an input.
+/// </summary>
+public class RadioInputExample : MonoBehaviour
+{
+    private RadioController controller; //radio controller reference
+    private bool triggerEntered = false;
+    public bool InvertradioDirection = false;
+
+    /// <summary>
+    /// Awake is called before Start.
+    /// </summary>
+    private void Awake()
+    {
+        TryGetComponent(out controller);    //find radio controller script attached to this object
+
+        if (TryGetComponent(out controller))
+        {
+            Debug.Log("RadioController found");
+        }
+        else
+        {
+            Debug.Log("RadioController not found");
+        }
+    }
+
+    /// <summary>
+    /// Update is called once per frame.
+    /// </summary>
+    void Update()
+    {
+
+        if (controller != null && triggerEntered)
+        {
+            Debug.Log("NEXT");
+            if (InvertradioDirection == false)
+            {
+                controller.CurrentStationIndex += 1;
+            }
+            else
+            {
+                controller.CurrentStationIndex -= 1;
+            }
+           
+            triggerEntered = false;
+        }
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+
+        
+
+        if (controller != null && other.CompareTag("RadioTrigger"))
+        {
+            Debug.Log("NEXT");
+            if (InvertradioDirection == false)
+            {
+                controller.CurrentStationIndex += 1;
+            }
+            else 
+            {
+                Debug.Log("perious");
+                controller.CurrentStationIndex -= 1;
+            }
+        }
+
+
+       
+    }
+}
