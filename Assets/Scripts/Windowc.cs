151f515db18e89a382cf58adff32a3c042455494
nicholas <1024990@student.sae.edu.au>
2023-04-17 10:40:01 +0800

    Car edits

---
+++ Assets/Scripts/Windowc.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Windowc : MonoBehaviour
+{
+    public GameObject window;
+    public GameObject ghosthand;
+    public Animator anime;
+    public float delay = 2f;
+   
+
+    public Animator ghosthandAnim;
+    
+
+
+    public void Start()
+    {
+        anime.Play("windowidle");
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("RadioTrigger"))
+        {
+            anime.Play("Window");
+            ghosthandAnim.Play("Ghost_Hand_slip");
+            Invoke("DeactivateGhostHand", delay);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("RadioTrigger"))
+        {
+            anime.Play("windowidle");
+        }
+    }
+
+    private void DeactivateGhostHand()
+    {
+        ghosthand.SetActive(false);
+    }
+}
