967eef5e847320b89aea75f902bd5d2f4cae5646
Dylan <dmand032@gmail.com>
2023-04-21 13:25:26 +0800

    UI almost done

---
+++ Assets/Scripts/LoseState.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class LoseState : MonoBehaviour
+{
+   public float raycastDistance = 1.0f; // The distance the raycast will travel
+    public string raycastTag = "death"; // The tag to filter the raycast
+
+    public FMODUnity.StudioEventEmitter emitter;
+
+    public GameObject LoseScreen;
+
+    void Update()
+    {
+        // Shoot a raycast down from the player
+        RaycastHit hitInfo;
+       
+        if (Physics.Raycast(transform.position, Vector3.down, out hitInfo, raycastDistance))
+        {
+            // The raycast hit something
+            if (hitInfo.collider.CompareTag(raycastTag))
+            {
+                Lose();
+                Debug.Log("Hit object: " + hitInfo.collider.gameObject.name);
+            }
+            else
+            {
+                // The raycast hit something that doesn't have the specified tag
+                Debug.Log("Hit object does not have the specified tag.");
+            }
+        }
+        else
+        {
+            // The raycast did not hit anything
+            Debug.Log("Did not hit anything.");
+        }
+    }
+
+    void Lose()
+    {
+        LoseScreen.SetActive(true);
+        //emitter.play();
+        Time.timeScale=0; 
+
+    }
+
+}
