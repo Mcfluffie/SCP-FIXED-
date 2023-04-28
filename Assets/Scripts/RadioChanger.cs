38b2275486f479e4b49006f59bf3419c1cb9777e
nicholas <1024990@student.sae.edu.au>
2023-04-28 15:30:14 +0800

    radio edits

--- Assets/Scripts/RadioChanger.cs
+++ Assets/Scripts/RadioChanger.cs
@@ -5,22 +5,18 @@ using FMOD;
 
 public class RadioChanger : MonoBehaviour
 {
-
     public FMODUnity.StudioEventEmitter myEventEmittertoplay;
     public FMODUnity.StudioEventEmitter HorrorRadio;
-
 
     [SerializeField]
     public FMODUnity.StudioEventEmitter myEventEmittertostop;
     public FMODUnity.StudioEventEmitter myEventEmittertostop2;
     public FMODUnity.StudioEventEmitter myEventEmittertostop3;
 
-
-
-
     // Start is called before the first frame update
     void Start()
     {
+        // Start playing the initial station
         
     }
 
@@ -34,16 +30,24 @@ public class RadioChanger : MonoBehaviour
     {
         if (other.CompareTag("RadioTrigger"))
         {
-            myEventEmittertoplay.Play();
+            // Stop the current station
+            myEventEmittertoplay.Stop();
             myEventEmittertostop.Stop();
             myEventEmittertostop2.Stop();
             myEventEmittertostop3.Stop();
 
-
+            // Randomly choose whether to play the HorrorRadio or the initial station
+            bool playHorrorRadio = Random.value < 0.5f;
+            if (playHorrorRadio)
+            {
+                HorrorRadio.Play();
+            }
+            else
+            {
+                myEventEmittertoplay.Play();
+            }
         }
-    
-
     }
+
 }
-     
 
